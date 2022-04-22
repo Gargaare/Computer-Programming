ea4f18f674a26bac0eebc10abd018bc704bb6055
Suleiman M. A. Gargaare <65114273+Gargaare@users.noreply.github.com>
2022-04-22 17:21:55 +0300

    Add files via upload

---
+++ input_Output.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+	int number;
+	
+	cout << "Please enter an integer number: " <<endl;
+	cin >> number;
+	
+	cout << "Your integer number is: " << number <<endl;
+	
+	return 0;
+}
