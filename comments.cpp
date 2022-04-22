8c593b87313454673cf740d88b5af3b2bb4507e8
Suleiman M. A. Gargaare <65114273+Gargaare@users.noreply.github.com>
2022-04-22 17:33:45 +0300

    Add files via upload

---
+++ comments.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+/*
+     This 
+      is
+     Your
+     First
+      C++
+    Program
+*/
+using namespace std;
+int main(){
+	
+	cout <<"Hello World." <<endl; // This is a line of code that prints Hello World
+	
+	return 0;
+	
+}
